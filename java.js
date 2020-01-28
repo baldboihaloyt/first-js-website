b83698b040d68f010ea0d8560eb48fc1f2515a6a
baldboihaloyt <59394520+baldboihaloyt@users.noreply.github.com>
2020-01-28 18:00:12 -0500

    Create java.js
    meme machine is changing

---
+++ java.js
@@ -0,0 +1,10 @@
+function update_text(){
+  var user_text = document.getElementById("user_text");
+  var meme_text = document.getElementById("meme_text");
+  meme_text.innerHTML = user_text.value;
+}
+function update_image(){
+  var img = document.querySelector('img');
+  var file = document.querySelector('input[type=file]').files[0];
+  img.src =  window.URL.createObjectURL(file);
+}
